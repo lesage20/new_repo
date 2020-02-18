fc7dc4a632423d0a3ea17b1fee3ddc7d35f6b36c
lesage20 <41262044+lesage20@users.noreply.github.com>
2020-02-18 11:44:20 +0000

    ajout de la fonction quit

--- test.py
+++ test.py
@@ -2,19 +2,21 @@
 # coding:utf-8
 
 import tkinter
-
+import sys
 app = Tk()
 #ce code est bizarre qui fait ça??
 app.config(bg="#550000")
 app.geometry("800x600")
-
+def quit:
+  sys.exit("messagebox.showinfo(sys.platform")
+  
 label = tkinter.Label(app, text="Hello I'am the sage", font="Arial 14 bold")
 label.pack(padx=10, pady=10, ipady=5)
 
-label = tkinter.Label(app, text="Me is Kevin", font="Arial 14 bold")
+label = tkinter.Label(app, text="I am Kevin Elysée ", font="Arial 14 bold")
 label.pack(padx=10, pady=10, ipady=5)
 
-bouton=Button(fenetre, text="Fermer", command=fenetre.quit)
+bouton=Button(fenetre, text="Fermer", command=quit)
 bouton.pack()
 
 app.mainloop()
