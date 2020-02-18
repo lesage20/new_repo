f2cfbe63734ce6e9b960fb338852ca35e37baa56
lesage20 <41262044+lesage20@users.noreply.github.com>
2020-02-18 12:00:13 +0000

    mise a jour mineur et ajout de commentaires

--- test.py
+++ test.py
@@ -3,20 +3,27 @@
 
 import tkinter
 import sys
+
+#creation de l'interface primaire
 app = Tk()
 #ce code est bizarre qui fait ça??
 app.config(bg="#550000")
 app.geometry("800x600")
-def quit:
-  sys.exit("messagebox.showinfo(sys.platform")
+
+#fonctions
+def quit():
+  sys.exit("messagebox.showinfo(sys.platform"))
   
+  
+# creation des widgets
 label = tkinter.Label(app, text="Hello I'am the sage", font="Arial 14 bold")
-label.pack(padx=10, pady=10, ipady=5)
-
-label = tkinter.Label(app, text="I am Kevin Elysée ", font="Arial 14 bold")
-label.pack(padx=10, pady=10, ipady=5)
-
+label1 = tkinter.Label(app, text="I am Kevin Elysée ", font="Arial 14 bold")
 bouton=Button(fenetre, text="Fermer", command=quit)
+
+#Affichage des widgets
+label.pack(padx=10, pady=10, ipady=5)
+label1.pack(padx=10, pady=10, ipady=5)
 bouton.pack()
 
+#boucle principale
 app.mainloop()
