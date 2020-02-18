ca07e8025576ce69f01f99ea510622c521565eb2
elysee-01 <61144357+elysee-01@users.noreply.github.com>
2020-02-18 11:26:24 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
 import tkinter
 
-app = tkinter.Tk()
+app = Tk()
 
 
-label = tkinter.Label(app, text="Hello I'am the sage")
+label = tkinter.Label(app, text="Hello I'am the sage", font="Arial 14 bold")
+label.pack(padx=10, pady=10, ipady=5)
 
-label.pack() 
+label = tkinter.Label(app, text="Me is Kevin", font="Arial 14 bold")
+label.pack(padx=10, pady=10, ipady=5)
 
 app.mainloop()
